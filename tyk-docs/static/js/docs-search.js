67b260fe2abb886bfa94ee7af7d2a93a700c6fd6
Leonid Bugaev <leonsbox@gmail.com>
2024-06-18 12:05:00 +0300

    Merging to release-5.2: [DX-123] add algolia docsearch v3 (#4761)
    [DX-123] add algolia docsearch v3 (#4761)
    
    * add v3

--- tyk-docs/static/js/docs-search.js
+++ tyk-docs/static/js/docs-search.js
@@ -5,19 +5,15 @@
    * INIT INSTANT SEARCH
    */
   docsearch({
-    algoliaOptions: {
-    hitsPerPage: 200,
-    // See https://www.algolia.com/doc/api-reference/api-parameters/
-    },
     // Your apiKey and indexName will be given to you once
     // we create your config
-    apiKey: 'ALGOLIA_API_KEY',
-    indexName: 'docsearch',
-    appId: 'ALGOLIA_APP_ID', // Should be only included if you are running DocSearch on your own.
+    apiKey: "ALGOLIA_NEW_API_KEY",
+    indexName: "tyk",
+    appId: "ALGOLIA_NEW_APP_ID", // Should be only included if you are running DocSearch on your own.
     // Replace inputSelector with a CSS selector
     // matching your search input
-    inputSelector,
-    // Set debug to true to inspect the dropdown
+    container: "#search_container",
+    // Set debug to true to inspect the dropdow
     // debug: true,
   });
 
